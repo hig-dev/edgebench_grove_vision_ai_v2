89be6524f150d6c0314aa76d7b8daefd43ced701
Halil Göcer <dev@hig.dev>
2025-06-16 01:45:29 +0200

    Change default

--- EPII_CM55M_APP_S/app/scenario_app/allon_sensor_tflm_freertos/common_config.h
+++ EPII_CM55M_APP_S/app/scenario_app/allon_sensor_tflm_freertos/common_config.h
@@ -16,7 +16,7 @@
  *		use flash memory mapped address to load model.
  *		in this example, model data is pre-burn to flash address: 0x180000
  * **/
-#define IS_EFFICIENTVIT             1       // 0 : is mobileone_s0, 1 : is efficientvit_b0
+#define IS_EFFICIENTVIT             0       // 0 : is mobileone_s0, 1 : is efficientvit_b0
 #define FLASH_XIP_MODEL             1
 #define MODEL_FLASH_ADDR            0x3AB7B000
 #define MEM_FREE_POS                (BOOT2NDLOADER_BASE)
